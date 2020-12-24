98326c13f4870f8bbf39b4b4b8fcb758335aa4d6
xuanhe <506078418@qq.com>
2020-12-24 17:24:49 +0800

    keepgoning

--- src/多线程/DevlopTest.java
+++ src/多线程/DevlopTest.java
@@ -3,5 +3,6 @@ package 多线程;
 public class DevlopTest {
     public static void main(String[] args) {
         System.out.println("keep going devlop");
+        System.out.println("keep going devlop2");
     }
 }
