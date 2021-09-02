2963455c0db46ebe81cc3c9d66ab4f49b47fb625
xuanhe <506078418@qq.com>
2021-09-03 00:41:24 +0800

    更新

---
+++ src/leetcodeHOT100/_84_柱状图中最大的矩形.java
@@ -0,0 +1,38 @@
+package leetcodeHOT100;
+//知耻而后勇，知弱而图强
+//给定 n 个非负整数，用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1 。
+//
+//        求在该柱状图中，能够勾勒出来的矩形的最大面积。
+//
+//         
+//
+//        示例 1:
+//
+//
+//
+//        输入：heights = [2,1,5,6,2,3]
+//        输出：10
+//        解释：最大的矩形为图中红色区域，面积为 10
+//        示例 2：
+//
+//
+//
+//        输入： heights = [2,4]
+//        输出： 4
+//         
+//
+//        提示：
+//
+//        1 <= heights.length <=105
+//        0 <= heights[i] <= 104
+public class _84_柱状图中最大的矩形 {
+    //1.暴力解法
+    public int largestRectangleArea(int[] heights) {
+        return 0;
+    }
+
+    //2.单调栈
+    public int largestRectangleAreaStack(int[] heights) {
+        return 0;
+    }
+}
