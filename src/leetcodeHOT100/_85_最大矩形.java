df98ab92d77833fc84e8f92242126a483ab5e088
xuanhe <506078418@qq.com>
2021-10-17 22:44:39 +0800

    更新

--- src/leetcodeHOT100/_85_最大矩形.java
+++ src/leetcodeHOT100/_85_最大矩形.java
@@ -6,6 +6,8 @@ import java.util.concurrent.locks.ReentrantLock;
 import java.util.regex.Pattern;
 
 public class _85_最大矩形 {
+    //单调栈，跟8题4差不多
+    //从上往下遍历，用一个数组记录当前行1的高度，然后使用84题的单调栈解决问题
     public int maximalRectangle(char[][] matrix) {
         if (matrix.length==0||matrix[0].length==0){
             return 0;
@@ -48,62 +50,4 @@ public class _85_最大矩形 {
     }
 
 
-    class Bank {
-
-        private long[] balance;
-        private int length;
-        public Bank(long[] balance) {
-            this.balance=balance;
-            this.length=balance.length;
-        }
-
-        public boolean transfer(int account1, int account2, long money) {
-            if (account1>length||account2>length||balance[account1]<money){
-                return false;
-            }
-            balance[account1-1]-=money;
-            balance[account2-1]+=money;
-            return true;
-        }
-
-        public boolean deposit(int account, long money) {
-            if (account>length){
-                return false;
-            }
-            balance[account-1]+=money;
-            return true;
-        }
-
-
-        public boolean withdraw(int account, long money) {
-            if (account>length||balance[account-1]<money){
-                return false;
-            }
-            balance[account-1]-=money;
-            return true;
-        }
-    }
-
-    int count=0;
-
-    public int countMaxOrSubsets(int[] nums) {
-        if (nums.length==1){
-            return 1;
-        }
-        int max=0;
-        for (int num : nums) {
-            max|=num;
-        }
-        dfs(nums,0,0,max);
-        return count;
-    }
-
-    public void dfs(int[] nums,int index,int num,int max){
-        if (num==max){
-            count++;
-        }
-        for (int i=index;i<nums.length;i++){
-            dfs(nums,i+1,num|nums[i],max);
-        }
-    }
 }
